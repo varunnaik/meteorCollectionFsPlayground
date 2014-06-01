2fae54eb98d87f65e589e2a8d7554f0da8c28557
Varun Naik <varun.naik@gmail.com>
2014-06-01 19:00:15 +0530

    Generate different sizes of the image on upload.

--- common/common.js
+++ common/common.js
@@ -1,6 +1,36 @@
 /**
  * Created by varun on 1/6/14.
  */
+// Note: If potentially sensitive info goes here (such as keys) this should be protected.
+var profilePicsDir = "~/profile-pictures/";
 ProfilePics = new FS.Collection("profile-pictures", {
-    stores: [new FS.Store.FileSystem("profile-pictures", {path: "~/profile-pictures/"})]
+    stores: [
+        new FS.Store.FileSystem("profile-picture", {path: profilePicsDir+"original/"}),
+        new FS.Store.FileSystem("profile-picture-tiny", {
+            transformWrite: function(fileObj, readStream, writeStream) {
+                // Transform the image into a 23x25px thumbnail
+                gm(readStream, fileObj.name()).resize('23', '25').stream().pipe(writeStream);
+            },
+            path: profilePicsDir+"tiny/"
+        }),
+        new FS.Store.FileSystem("profile-picture-small", {
+            transformWrite: function(fileObj, readStream, writeStream) {
+                // Transform the image into a 64x72px thumbnail
+                gm(readStream, fileObj.name()).resize('64', '72').stream().pipe(writeStream);
+            },
+            path: profilePicsDir+"small/"
+        }),
+        new FS.Store.FileSystem("profile-picture-normal", {
+            transformWrite: function(fileObj, readStream, writeStream) {
+                // Transform the image into a full-size profile picture
+                gm(readStream, fileObj.name()).resize('171', '180').stream().pipe(writeStream);
+            },
+            path: profilePicsDir+"normal/"
+        })
+    ],
+    filter: {
+        allow: {
+            contentTypes: ['image/*'] //allow only images in this FS.Collection
+        }
+    }
 });
--- readme.md
+++ readme.md
@@ -3,3 +3,6 @@ This Meteor component supports the following:
 1. Upload an image
 2. Convert the image into several different standard sizes and qualities
 3. Store the image efficiently in MongoDb
+
+This will create a directory in your home directory, profile-pics and store resized versions of the uploaded
+images into the original, normal, small, tiny subdirectories within that directory.
