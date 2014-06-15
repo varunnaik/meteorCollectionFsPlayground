6d7ebc31831276aa6a986e28469195379a657212
Varun Naik <varun.naik@gmail.com>
2014-06-15 15:28:19 +0530

    Use GridFS instead of local FS for storing images.

--- common/common.js
+++ common/common.js
@@ -3,24 +3,25 @@
  */
 // Note: If potentially sensitive info goes here (such as keys) this should be protected.
 // Images are stored in ../.meteor/local/cfs/files
+
 ProfilePics = new FS.Collection("profile-pictures", {
     stores: [
-        new FS.Store.FileSystem("profile-picture-original"),
-        new FS.Store.FileSystem("profile-picture-tiny", {
+        new FS.Store.GridFS("profile-picture-original"),
+        new FS.Store.GridFS("profile-picture-tiny", {
             transformWrite: function(fileObj, readStream, writeStream) {
                 // Transform the image into a 23x25px thumbnail
                 gm(readStream, fileObj.name()).resize('23', '25').stream().pipe(writeStream);
             }
         }),
 
-        new FS.Store.FileSystem("profile-picture-small", {
+        new FS.Store.GridFS("profile-picture-small", {
             transformWrite: function(fileObj, readStream, writeStream) {
                 // Transform the image into a 64x72px thumbnail
                 gm(readStream, fileObj.name()).resize('64', '72').stream().pipe(writeStream);
             }
         }),
 
-        new FS.Store.FileSystem("profile-picture-normal", {
+        new FS.Store.GridFS("profile-picture-normal", {
             transformWrite: function(fileObj, readStream, writeStream) {
                 // Transform the image into a full-size profile picture
                 gm(readStream, fileObj.name()).resize('171', '180').stream().pipe(writeStream);
